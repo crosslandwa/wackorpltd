b08b5b97d805f69241ed686e4f878f861dc92c45
will.crossland <will.crossland@defra.onmicrosoft.com>
2025-08-13 23:35:07 +0100

    Resolve assets when deployed to custom domain

--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,7 @@ const config = {
       fallback: '404.html',
       precompress: false,
       strict: true
-    }),
-    paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-    }
+    })
   }
 };
 
